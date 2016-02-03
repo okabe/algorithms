2645beb7538cf762b947b7b67a0ae3a4461a6eb9
mikepicard <mikepicard01@gmail.com>
2016-02-03 00:05:15 -0500

    Update eratosthenes.py

--- eratosthenes.py
+++ eratosthenes.py
@@ -2,7 +2,6 @@
 #author: mp
 #comment: snag all prime numbers up to a max value set by sys.argv[1]
 #         https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes 
-
 import sys
 
 n = int( sys.argv[1] )
@@ -11,7 +10,6 @@ for i in range( 2, n + 1 ):
     for j in range( 2, n + 1 / i ):
         if i * j <= n:
             ignore.append( ( i * j ) )
-
 for i in range( 2, n + 1 ):
     if i not in ignore:
         print i
