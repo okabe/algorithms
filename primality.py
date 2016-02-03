17bc611d8bfb7a089a44d21f749718abb37a9544
mikepicard <mikepicard01@gmail.com>
2016-02-03 00:02:28 -0500

    Update primality.py

--- primality.py
+++ primality.py
@@ -13,13 +13,12 @@ if n <= 1:
     print "{} is less than 1, not prime".format( str( n ) )
     isprime = False
 elif n <= 3:
-    print "{} is prime".format( str( n ) )
-    isprime = False
+    isprime = True
 elif n % 2 == 0 or n % 3 == 0:
     print "{} is divisible by 2, 3, not prime".format( str( n ) )
     isprime = False
 else:
-    while i <= ( n / 2 ):
+    while i <= n:
         if n % i == 0:
             print "{} is divisible by {}, not prime".format( str( n ), str( i ) )
             isprime = False
