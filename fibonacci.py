a1571b25def597deb37b7cd98ff63d1d4e835798
mikepicard <mikepicard01@gmail.com>
2016-02-03 09:40:44 -0500

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,12 @@
 #!/bin/env python2
 #author: mp
 #comment: all numbers in fibonacci sequence less than sys.argv[1]
-
+#         https://en.wikipedia.org/wiki/Fibonacci_number
 import sys
 
 f  = 0
 f1 = 0
 f2 = 1
-
 while f <= int( sys.argv[1] ):
     print f
     f  = f1 + f2
